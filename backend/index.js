88fc60214e33c6f7d8994bf9f46dfa356549a35d
Sudir Krishnaa RS <sudirkrish17@gmail.com>
2026-02-23 15:31:18 +0530

    removed comments and formatted code

--- backend/index.js
+++ backend/index.js
@@ -1,4 +1,5 @@
 const express = require("express");
+const PORT = 5000;
 const cors = require("cors");
 const walletRoute = require("./routes/wallet");
 
@@ -6,27 +7,7 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 
-// JSON error handling middleware
-// app.use((err, req, res, next) => {
-
-//   if (err instanceof SyntaxError && err.status === 400 && "body" in err) {
-//     return res.status(400).json({
-//       error: "Invalid JSON format",
-//       message:
-//         "Please check your JSON syntax and ensure all property names are in double quotes",
-//     });
-//   }
-//   next();
-// });
-
-const router = express.Router();
-
-// router.get("/test", (req, res) => {
-//   res.status(200).json({ message: "Wallet API is working" });
-// });
-
+// ROUTES
 app.use("/api/wallet", walletRoute);
 
-app.listen(5000, () => {
-  console.log("Server running on port 5000");
-});
+app.listen(5000, () => console.log(`Server running on port ${PORT}`));
